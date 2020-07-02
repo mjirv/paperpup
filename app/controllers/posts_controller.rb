bae0ddf0c109a953e49916d7dc24e7372733f8c0
Michael Irvine <michael.j.irvine@gmail.com>
2020-07-02 13:56:23 -0400

    add infinite scroll

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,13 @@
 class PostsController < ApplicationController
   def index
     source_id = params[:source_id] || CollectionSource.where(collection_id: params[:collection_id]).select(:source_id).distinct.pluck(:source_id)
-    @posts = Post.where(source_id: source_id).where.not(post_timestamp: nil).order('post_timestamp desc').limit(params[:limit]).map(&:get_attrs_to_show)
+    start_time = params[:start_time] || -Float::INFINITY
+    @posts = Post.where(source_id: source_id)
+      .where.not(post_timestamp: nil)
+      .where(post_timestamp: start_time...Float::INFINITY)
+      .order('post_timestamp desc')
+      .limit(params[:limit])
+      .map(&:get_attrs_to_show)
     render json: @posts
   end
 
